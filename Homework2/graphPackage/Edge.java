23092ee274465adda8a8daa3507961d98a3c872e
sivannachum <43347661+sivannachum@users.noreply.github.com>
2021-02-28 08:29:06 -0500

    Add files via upload

---
+++ Homework2/graphPackage/Edge.java
@@ -0,0 +1,89 @@
+//-----------------------------------------------------
+// Author: 		Sivan Nachum
+// Date: 		Feb 26, 2021
+// Description:	Java code to represent an edge in an undirected graph
+//-----------------------------------------------------
+// Referenced Java oracle doc on Comparable interface: https://docs.oracle.com/javase/8/docs/api/java/lang/Comparable.html
+public class Edge implements Comparable<Edge> {
+    int u;
+    int v;
+    
+    //-------------------------------------
+    // Constructor
+    // Name:    Edge
+    // Input: 	the vertices to connect
+    // Output:	none
+    //          creates an Edge object to represent an edge between these vertices
+    //-------------------------------------
+    public Edge(int u, int v){
+        if (u < v){
+            this.u = u;
+            this.v = v;
+        }
+        else{
+            this.u = v;
+            this.v= u;
+        }
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    getFirstVertex
+    // Input: 	none
+    // Output:	the first vertex in the edge
+    //-------------------------------------
+    public int getFirstVertex(){
+        return u;
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    getSecondVertex
+    // Input: 	none
+    // Output:	the second vertex in the edge
+    //-------------------------------------
+    public int getSecondVertex(){
+        return v;
+    }
+
+    //-------------------------------------
+    // Function - referenced Java oracle doc on Sets to realize that I could use make a HashSet<Edge> if I implemented this method
+    //            (for some reason that didn't actually work out, but I still use this method nonetheless for checking if a graph is simple)
+    //            https://docs.oracle.com/javase/7/docs/api/java/util/Set.html
+    // Name:    equals
+    // Input: 	the edge to compare this one to
+    // Output:	true if the edges are equal, false otherwise
+    //-------------------------------------
+    public boolean equals(Object obj){
+        Edge connection = (Edge)obj;
+        return (this.getFirstVertex() == connection.getFirstVertex()) && (this.getSecondVertex() == connection.getSecondVertex());
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    compareTo
+    // Input: 	the edge to compare this one with
+    // Output:	-1 if this edge is less than the input edge, 1 if it is greater, 0 if they are the same edge
+    //-------------------------------------
+    @Override
+    public int compareTo(Edge connection){
+        if (this.getFirstVertex() < connection.getFirstVertex()){
+            return -1;
+        }
+        else if (this.getFirstVertex() == connection.getFirstVertex()){
+            if (this.getSecondVertex() < connection.getSecondVertex()){
+                return -1;
+            }
+            else if (this.getSecondVertex() == connection.getSecondVertex()){
+                return 0;
+            }
+            else {
+                return 1;
+            }
+        }
+        else{
+            return 1;
+        }
+    }
+
+}
---
+++ Homework2/graphPackage/GraphEdgeList.java
@@ -0,0 +1,399 @@
+//-----------------------------------------------------
+// Author: 		Sivan Nachum
+// Date: 		Feb 26, 2021
+// Description:	Java code to create an undirectional Graph representation via an edge list
+//-----------------------------------------------------
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.Collections;
+
+public class GraphEdgeList {
+    private int vertices;
+    private ArrayList<Edge> edges;
+
+    //-------------------------------------
+    // Constructor
+    // Name:    GraphEdgeList
+    // Input: 	none
+    // Output:	none
+    //          creates an object of type GraphEdgeList with no vertices or edges
+    //-------------------------------------
+    public GraphEdgeList(){
+        this.vertices = 0;
+        this.edges = new ArrayList<Edge>();
+    }
+
+    //-------------------------------------
+    // Constructor
+    // Name:    GraphEdgeList
+    // Input: 	the number of vertices for the graph
+    // Output:	none
+    //          creates an object of type GraphEdgeList with the specified number of vertices
+    //-------------------------------------
+    public GraphEdgeList(int vertices){
+        this.vertices = vertices;
+        this.edges = new ArrayList<Edge>();
+    }
+
+    //-------------------------------------
+    // Constructor
+    // Name:    GraphEdgeList
+    // Input: 	the number of vertices and edges for the graph
+    // Output:	none
+    //          creates an object of type GraphEdgeList with the specified number of vertices and edges; the graph is random
+    //-------------------------------------
+    public GraphEdgeList(int vertices, int numEdges){
+        this.vertices = vertices;
+        this.edges = new ArrayList<Edge>();
+        if (vertices != 0){
+            int i = 0;
+            int u = 0;
+            int w = 0;
+            Random rand = new Random();
+            while (i < numEdges){
+                u = rand.nextInt(vertices);
+                w = rand.nextInt(vertices);
+                Edge connection = new Edge(u, w);
+                edges.add(connection);
+                i++;
+            }
+        }
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    getNumVertices
+    // Input: 	none
+    // Output:	the number of vertices in the graph
+    //-------------------------------------
+    public int getNumVertices(){
+        return vertices;
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    setNumVertices
+    // Input: 	the new number of vertices for the graph
+    // Output:	none
+    //          sets the number of vertices to the given input, creates a new cleared edge list
+    //-------------------------------------
+    public void setNumVertices(int numVertices){
+        vertices = numVertices;
+        edges = new ArrayList<Edge>();
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    addVertices
+    // Input: 	the number of vertices to add
+    // Output:	none
+    //          adds num vertices to the graph
+    //-------------------------------------
+    public void addVertices(int num){
+        vertices += num;
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    deleteVertex
+    // Input: 	the number of the vertex to delete
+    // Output:	none
+    //          deletes the vertex from the graph
+    //-------------------------------------
+    public void deleteVertex(int num){
+        vertices -= 1;
+        for (int i = edges.size()-1; i >= 0; i--){
+            Edge connection = edges.get(i);
+            if (connection.getFirstVertex() == num || connection.getSecondVertex() == num){
+                edges.remove(i);
+            }
+            else if (connection.getFirstVertex() > num && connection.getSecondVertex() > num){
+                edges.remove(i);
+                edges.add(new Edge(connection.getFirstVertex()-1, connection.getSecondVertex()-1));
+            }
+            else if (connection.getFirstVertex() > num){
+                edges.remove(i);
+                edges.add(new Edge(connection.getFirstVertex()-1, connection.getSecondVertex()));
+            }
+            else if (connection.getSecondVertex() > num){
+                edges.remove(i);
+                edges.add(new Edge(connection.getFirstVertex(), connection.getSecondVertex()-1));
+            }
+        }
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    getEdgeList
+    // Input: 	none
+    // Output:	the graph's edge list
+    //-------------------------------------
+    public ArrayList<Edge> getEdgeList(){
+        return edges;
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    setEdgeList
+    // Input: 	an edge list
+    // Output:	none
+    //          sets the graph's edge list to the given input; note that the number of vertices is not adjusted
+    //-------------------------------------
+    public void setEdgeList(ArrayList<Edge> newList){
+        edges = newList;
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    addEdge
+    // Input: 	two vertices u and v
+    // Output:	none
+    //          adds an undirected edge between the vertices u and v in the edge list
+    //-------------------------------------
+    public void addEdge(int u, int v){
+        Edge connection = new Edge(u, v);
+        edges.add(connection);
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    removeEdge
+    // Input: 	two vertices u and v
+    // Output:	none
+    //          remove one undirected edge between the vertices u and v in the edge list
+    //-------------------------------------
+    public void removeEdge(int u, int v){
+        if (u > v){
+            int temp = u;
+            u = v;
+            v = temp;
+        }
+        int i = 0;
+        for (i = 0; i < edges.size(); i++){
+            Edge connection = edges.get(i);
+            if (connection.getFirstVertex() == u && connection.getSecondVertex() == v){
+                break;
+            }
+        }
+       if (i != edges.size()){
+           edges.remove(i);
+       }
+    }
+
+    //-------------------------------------
+    // Function - this could be sped up with binary search, which is the original reason I wanted
+    //            to implement the Comparable interface for Edge but currently I am just using sequential search
+    // Name:    hasEdge
+    // Input: 	two vertices u and v
+    // Output:	true if the vertices have an edge between them, false otherwise
+    //-------------------------------------
+    public boolean hasEdge(int u, int v){
+        if (u > v){
+            int temp = u;
+            u = v;
+            v = temp;
+        }
+        for (Edge connection : edges){
+            if (connection.getFirstVertex() == u && connection.getSecondVertex() == v){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    isEmpty
+    // Input: 	none
+    // Output:	true if the graph is empty (has no vertices or edges), false otherwise
+    //-------------------------------------
+    public boolean isEmpty(){
+        return vertices == 0;
+        /*
+        return edges.isEmpty();
+        */
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    isSimple
+    // Input: 	none
+    // Output:	true if the graph is simple (has no self-loops or multiple edges between vertices), false otherwise
+    //-------------------------------------
+    public boolean isSimple(){
+        boolean simple = true;
+        Collections.sort(edges);
+
+        Edge prev = null;
+        for (int i = 0; i < edges.size(); i++){
+            // contains self-loop
+            if (edges.get(i).getFirstVertex() == edges.get(i).getSecondVertex()){
+                simple = false;
+                break;
+            }
+            // contains multi-edge
+            if (prev != null && edges.get(i).equals(prev)){
+                simple = false;
+                break;
+            }
+            prev = edges.get(i);
+        }
+
+        return simple;
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    printGraph
+    // Input: 	none
+    // Output:	none
+    //          prints out the edge list representation of the graph
+    //-------------------------------------
+	public void printGraph(){
+        String[] toPrint = new String[vertices];
+        for (int vertex = 0; vertex < vertices; vertex++) { 
+            toPrint[vertex] = "Edges exist from vertex " + vertex + " to: ";
+        }
+        for (Edge connection : edges) { 
+            toPrint[connection.getFirstVertex()] +=  connection.getSecondVertex() + " ";
+            if (connection.getFirstVertex() != connection.getSecondVertex()){
+                toPrint[connection.getSecondVertex()] += connection.getFirstVertex() + " ";
+            }
+        }
+        for (String s : toPrint){
+            System.out.println(s);
+        }
+	}
+
+    //-------------------------------------
+    // Function
+    // Name:    toString
+    // Input: 	none
+    // Output:	a String representing the graph
+    //-------------------------------------
+    public String toString(){
+        String result = "{\n" + vertices + ",\n{\n";
+        boolean firstElem = true;
+        for (Edge connection : edges){
+            if (!firstElem) {
+                result += ",";
+            } else {
+                firstElem = false;
+            }
+            result += "{" + (connection.getFirstVertex()+1) + "," + (connection.getSecondVertex()+1) + "}";
+        }
+        result += "\n}\n}";
+        return result;
+    }
+
+    //-------------------------------------
+    // Function - referenced w3schools code for basic file I/O at https://www.w3schools.com/java/java_files_create.asp
+    // Name:    writeToFile
+    // Input: 	none
+    // Output:	none
+    //          writes the serialized version of the graph to a file
+    //-------------------------------------
+    public void writeToFile(String filename){
+        try {
+            FileWriter myWriter = new FileWriter(filename);
+            myWriter.write(toString());
+            myWriter.close();
+          } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+    }
+
+    //-------------------------------------
+    // Function - referenced w3schools code for basic file I/O at https://www.w3schools.com/java/java_files_read.asp
+    // Name:    readFromFile
+    // Input: 	the name of the file to read from
+    // Output:	none
+    //          creates a graph from the information in the file
+    //-------------------------------------
+    public void readFromFile(String filename){
+        try {
+            File graphFile = new File(filename);
+            Scanner myReader = new Scanner(graphFile);
+            // The first line is just a bracket, so we can skip it
+            myReader.nextLine();
+            // The second line has the number of vertices
+            String data = myReader.nextLine();
+            String cleanData = "";
+            for (int i = 0; i < data.length()-1; i++){
+                cleanData += data.charAt(i);
+            }
+            // Initialize the graph with the proper number of vertices
+            setNumVertices(Integer.parseInt(cleanData));
+
+            // The next line is just a bracket, so we can skip it
+            myReader.nextLine();
+            // The next line is the edges
+            data = myReader.nextLine();
+            String u = "";
+            String v = "";
+            boolean buildingU = true;
+
+            for (int i = 0; i < data.length(); i++){
+                if (data.charAt(i) == '{'){
+                    buildingU = true;
+                    u = "";
+                    v = "";
+                }
+                else if (data.charAt(i)=='}'){
+                    addEdge(Integer.parseInt(u)-1, Integer.parseInt(v)-1);
+                }
+                else if (data.charAt(i)==','){
+                    buildingU = false;
+                }
+                else if (buildingU){
+                    u += data.charAt(i);
+                }
+                else {
+                    v += data.charAt(i);
+                }
+            }
+            // At this point, we have gotten all the information we need from the file
+            myReader.close();
+          } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+          }
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    convertToAdjMatrix
+    // Input: 	none
+    // Output:	a GraphAdjMatrix representation of the same graph
+    //          Note: multi-edges will be lost
+    //-------------------------------------
+    public GraphAdjMatrix convertToAdjMatrix(){
+        GraphAdjMatrix adjMatrix = new GraphAdjMatrix(vertices);
+        for (Edge connection : edges){
+            adjMatrix.addEdge(connection.getFirstVertex(), connection.getSecondVertex());
+        }
+        return adjMatrix;
+    }
+
+    //-------------------------------------
+    // Function
+    // Name:    convertToAdjList
+    // Input: 	none
+    // Output:	a GraphAdjList representation of the same graph
+    //-------------------------------------
+    public GraphAdjList convertToAdjList(){
+        GraphAdjList adjList = new GraphAdjList(vertices);
+        for (Edge connection : edges){
+            adjList.addEdge(connection.getFirstVertex(), connection.getSecondVertex());
+        }
+        return adjList;
+    }
+}
